2c879a91e8dbbc6341d0d7470e4f9ce5bb7602f1
desmondemmanuel020 <desmondemmanuel020@gmail.com>
2026-04-29 09:49:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+{
+  "name": "record-chief",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "build": "node build.js"
+  },
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.23.5",
+    "@babel/preset-env": "^7.23.5",
+    "@babel/preset-react": "^7.23.3"
+  }
+}
