40aedc78dea4a6f92674511e9bcfe681b0b7bb60
desmondemmanuel020 <desmondemmanuel020@gmail.com>
2026-05-03 23:55:34 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE="rc-v6";
-const CORE=["https://unpkg.com/react@18/umd/react.production.min.js","https://unpkg.com/react-dom@18/umd/react-dom.production.min.js","https://unpkg.com/@babel/standalone@7.23.5/babel.min.js"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(CORE.map(u=>c.add(u).catch(()=>{})))));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(c=>{if(c)return c;return fetch(e.request).then(r=>{if(r&&r.status===200&&r.type!=="opaque"){const cl=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,cl));}return r;}).catch(()=>e.request.mode==="navigate"?caches.match("/index.html"):undefined);}));});
-self.addEventListener("message",e=>{if(e.data&&e.data.type==="SKIP_WAITING")self.skipWaiting();});
